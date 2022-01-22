31444b8e720ea3d1196f8977aa1571b79815eec7
Jaxcoder <codenamejason@outlook.com>
2022-01-22 11:53:26 -0500

    Add token contract

---
+++ packages/hardhat/contracts/TalentDaoToken.sol
@@ -0,0 +1,16 @@
+pragma solidity >=0.8.0 <0.9.0;
+//SPDX-License-Identifier: MIT
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
+
+contract TalentDaoToken is Ownable {
+
+  constructor(address _owner) public ERC20("Talent DAO Token", "TDAO") {
+    _transferOwnership(_owner);
+  }
+
+}
