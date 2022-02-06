ab784ff193c103753beaec1f1ec935669a009dff
Jaxcoder <codenamejason@outlook.com>
2022-02-06 01:03:14 -0500

    need to work on the layout

--- packages/react-app/src/components/HelperComponents/LatestArticles.jsx
+++ packages/react-app/src/components/HelperComponents/LatestArticles.jsx
@@ -25,36 +25,34 @@ const responsive = {
 const LatestArticles = () => {
   return (
     <div className="w-full">
-      <div>
+      <div className="">
         <h2 className="flex ml-5 text-3xl">Latest Articles</h2>
+        {/* <span className="">favorites</span>
+        <span>following</span>
+        <span>
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="red">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z"
+            />
+          </svg>
+        </span>
+        <span>
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="red">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        </span> */}
         <hr
           className="max-w-xs font-bold mb-6"
           style={{ height: "1px", border: "none", color: "#333", backgroundColor: "#333" }}
         />
-        <span>favorites</span>
-        <span>following</span>
-        <span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
-          </svg>
-        </span>
-        <span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14 5l7 7m0 0l-7 7m7-7H3" />
-          </svg>
-        </span>
       </div>
 
       <Carousel responsive={responsive}>
