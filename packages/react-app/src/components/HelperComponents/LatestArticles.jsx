70c41fc41475b75824661acb5ed46ff06a02d484
Jaxcoder <codenamejason@outlook.com>
2022-02-06 00:59:22 -0500

    working on the Latest Articles nav

--- packages/react-app/src/components/HelperComponents/LatestArticles.jsx
+++ packages/react-app/src/components/HelperComponents/LatestArticles.jsx
@@ -2,30 +2,61 @@ import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import { ArticleCard } from "./ArticleCard";
 
-function LatestArticles() {
-  const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5,
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3,
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2,
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-    },
-  };
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
 
+const LatestArticles = () => {
   return (
     <div className="w-full">
-      <h2>Latest Articles</h2>
+      <div>
+        <h2 className="flex ml-5 text-3xl">Latest Articles</h2>
+        <hr
+          className="max-w-xs font-bold mb-6"
+          style={{ height: "1px", border: "none", color: "#333", backgroundColor: "#333" }}
+        />
+        <span>favorites</span>
+        <span>following</span>
+        <span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+          </svg>
+        </span>
+        <span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14 5l7 7m0 0l-7 7m7-7H3" />
+          </svg>
+        </span>
+      </div>
+
       <Carousel responsive={responsive}>
         <ArticleCard />
         <ArticleCard />
@@ -34,6 +65,6 @@ function LatestArticles() {
       </Carousel>
     </div>
   );
-}
+};
 
 export default LatestArticles;
