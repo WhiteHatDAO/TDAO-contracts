c83d4660f7c1caac25c5ef86bcce1524d1496256
Jaxcoder <codenamejason@outlook.com>
2022-02-06 00:26:25 -0500

    working on centering splash

--- packages/react-app/src/components/HelperComponents/Splash.jsx
+++ packages/react-app/src/components/HelperComponents/Splash.jsx
@@ -3,7 +3,7 @@ import InputWithButton from "./InputWithButton";
 /* This example requires Tailwind CSS v2.0+ */
 export default function Splash() {
   return (
-    <div className="relative bg-indigo-800">
+    <div className="relative bg-indigo-800 justify-center">
       <div className="absolute inset-0">
         {/* <img
                     className="w-full h-full object-cover"
@@ -16,15 +16,15 @@ export default function Splash() {
         <h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl">
           Journal of Decentralized Work
         </h1>
-        <p className="mt-6 text-xl text-indigo-100 max-w-3xl">
+        <h5 className="mt-6 text-xl text-indigo-100 max-w-3xl text-center mb-8">
           We are a new generation of researchers building the world's first decentralized community-reviewed publication
           protocol for the social sciences.
-        </p>
-        <div className="flex">
+        </h5>
+        <div className="flex -mb-10">
           <InputWithButton />
           <button
             type="button"
-            className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
+            className="px-4 py-1 mt-1 border border-transparent shadow-sm font-small rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
           >
             Submit Article
           </button>
