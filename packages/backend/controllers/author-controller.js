9dbec9e1da0d49aa0146b21a979b87f40e5ca9e7
Jaxcoder <codenamejason@outlook.com>
2022-02-25 16:10:12 -0500

    building controller out

--- packages/backend/controllers/author-controller.js
+++ packages/backend/controllers/author-controller.js
@@ -1,21 +1,43 @@
 const Author = require("../models/author-model");
 
-createAuthor = (req, res) => {
-    
-};
+createAuthor = (req, res) => {};
 
 deleteAuthor = (req, res) => {};
 
 updateAuthor = (req, res) => {};
 
-getAuthors = (req, res) => {};
+getAuthors = async (req, res) => {
+  await Author.find({}, (err, authors) => {
+    if (err) {
+      return res.status(400).json({ success: false, error: err });
+    }
+    if (!authors.length) {
+      return res
+        .status(404)
+        .json({ success: false, error: "Authors not found" });
+    }
+    return res.status(200).json({ success: true, data: authors });
+  }).catch((err) => console.error(err));
+};
 
-getAuthorById = (req, res) => {};
+getAuthorByWalletId = async (req, res) => {
+  await Author.findOne({ walletId: req.params.walletId }, (err, author) => {
+    if (err) {
+      return res.status(400).json({ success: false, error: err });
+    }
+    if (!author) {
+      return res
+        .status(404)
+        .json({ success: false, error: `Author not found` });
+    }
+    return res.status(200).json({ success: true, data: author });
+  }).catch((err) => console.error(err));
+};
 
 module.exports = {
   createAuthor,
   deleteAuthor,
   updateAuthor,
   getAuthors,
-  getAuthorById,
+  getAuthorByWalletId,
 };
--- packages/backend/routes/author-router.js
+++ packages/backend/routes/author-router.js
@@ -5,9 +5,9 @@ const AuthorController = require("../controllers/author-controller");
 const router = express.Router();
 
 router.post("/author", AuthorController.createAuthor);
-router.put("/author/id", AuthorController.updateAuthor);
+router.put("/author/walletId", AuthorController.updateAuthor);
 router.delete("/author/id", AuthorController.deleteAuthor);
 router.get("/authors", AuthorController.getAuthors);
-router.get("/authors/id", AuthorController.getAuthorById);
+router.get("/author/walletId", AuthorController.getAuthorByWalletId);
 
 module.exports = router;
