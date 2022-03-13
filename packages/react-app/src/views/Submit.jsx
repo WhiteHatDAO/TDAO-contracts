b5326d0c8557596d3ffed7bc0024fe9bdc980595
Jaxcoder <codenamejason@outlook.com>
2022-03-13 16:30:26 -0400

    juf;asjfdjaskl

--- packages/react-app/src/views/Submit.jsx
+++ packages/react-app/src/views/Submit.jsx
@@ -137,31 +137,6 @@ const Submit = ({ address, tx, writeContracts, readContracts }) => {
     // submitOnChain(arweaveHash);
   };
 
-  async function sendTransacton(data, arJWK) {
-    // console.log(arJWK);
-    let transaction = await arweave.createTransaction(
-      {
-        data: data,
-      },
-      arJWK,
-    );
-    // Examples
-    // transaction.addTag("Content-Type", "text/html");
-    transaction.addTag("key2", "value2");
-    console.log(transaction);
-  
-    await arweave.transactions.sign(transaction, arJWK);
-  
-    //   let uploader = await arweave.transactions.getUploader(transaction);
-  
-    //   while (!uploader.isComplete) {
-    //     await uploader.uploadChunk();
-    //     console.log(`${uploader.pctComplete}% complete, ${uploader.uploadedChunks}/${uploader.totalChunks}`);
-    //   }
-  
-    return transaction;
-  }
-
   const submitToArweave = async articleFile => {
     const arJWK = await generateWallet();
     console.log("arJWK", arJWK);
