ffcb2a9efc9c8b255780ffbe4d4513ecf941af4b
Jaxcoder <codenamejason@outlook.com>
2022-03-29 15:27:56 -0400

    more messing around with subgraph

--- packages/react-app/src/views/Subgraph.jsx
+++ packages/react-app/src/views/Subgraph.jsx
@@ -1,10 +1,8 @@
-import { gql, useQuery } from "@apollo/client";
 import "antd/dist/antd.css";
 import GraphiQL from "graphiql";
 import "graphiql/graphiql.min.css";
 import fetch from "isomorphic-fetch";
-import React, { useState } from "react";
-import { Address } from "../components";
+import React from "react";
 
 const highlight = {
   marginLeft: 4,
@@ -14,45 +12,45 @@ const highlight = {
   fontWeight: "bolder",
 };
 
-function Subgraph(props) {
+function Subgraph({ subgraphUri }) {
   function graphQLFetcher(graphQLParams) {
-    return fetch(props.subgraphUri, {
+    return fetch(subgraphUri, {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(graphQLParams),
     }).then(response => response.json());
   }
 
-  const EXAMPLE_GRAPHQL = `
-  query {
-    transactions(ids: ["fOI7gtDPUF5dWj_67shRx5r1YDRT5YOdcvPiNquQSaQ"]) {
+  const GET_TRANSACTION_BY_ID = `
+    query {
+      transactions(ids: ["fOI7gtDPUF5dWj_67shRx5r1YDRT5YOdcvPiNquQSaQ"]) {
         edges {
-            node {
-                id
-              	anchor
-                tags {
-                  name
-                  value
-                }
-              	data {
-                  size
-                  type
-                }
-              	quantity {
-                  winston
-                  ar
-                }
+          node {
+            id
+            anchor
+            tags {
+              name
+              value
             }
+            data {
+              size
+              type
+            }
+            quantity {
+              winston
+              ar
+            }
+          }
         }
+      }
     }
-}
   `;
 
   return (
     <>
       <div style={{ margin: "auto", paddingBottom: 64 }}>
         <div style={{ margin: 32, height: 600, border: "1px solid #888888", textAlign: "left" }}>
-          <GraphiQL fetcher={graphQLFetcher} docExplorerOpen query={EXAMPLE_GRAPHQL} />
+          <GraphiQL fetcher={graphQLFetcher} docExplorerOpen query={GET_TRANSACTION_BY_ID} />
         </div>
       </div>
 
