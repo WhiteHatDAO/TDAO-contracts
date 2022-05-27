e9d1761b87338902d7bc4aac031c72f736aa3b49
Jaxcoder <codenamejason@outlook.com>
2022-05-27 16:32:39 -0400

    couple file updates

--- packages/hardhat/contracts/arweave/ArweaveBase.sol
+++ packages/hardhat/contracts/arweave/ArweaveBase.sol
@@ -4,6 +4,6 @@ pragma solidity >=0.8.0 <0.9.0;
 import "hardhat/console.sol"; // Remove after testing is completed - JR
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ReputationBase is Ownable {
+contract ArweaveBase is Ownable {
     constructor () public {}
 }
