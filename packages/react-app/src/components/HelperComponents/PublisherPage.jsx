367229fca4a6a245b2a0f8ac3b47114c368b1934
Jaxcoder <codenamejason@outlook.com>
2022-07-18 18:47:30 -0400

    some updates

--- packages/react-app/src/components/HelperComponents/PublisherPage.jsx
+++ packages/react-app/src/components/HelperComponents/PublisherPage.jsx
@@ -55,7 +55,7 @@ const PublisherPage = ({ address }) => {
         </div>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-        <Suspense fallback={<div>Loading Publisher...</div>}>
+        <Suspense fallback={<div>Loading Publishers...</div>}>
           <PublisherCard></PublisherCard>
           <PublisherCard></PublisherCard>
           <PublisherCard></PublisherCard>
