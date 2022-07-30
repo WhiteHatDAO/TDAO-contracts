24421be636b1ed8194237d799a4487ae33f5fe28
Jaxcoder <codenamejason@outlook.com>
2022-07-30 12:28:09 -0400

    remove safe math from staking + other clean up

--- packages/hardhat/contracts/rTalentNftToken.sol
+++ packages/hardhat/contracts/rTalentNftToken.sol
@@ -4,20 +4,17 @@ pragma solidity >=0.8.0 <0.9.0;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 /// @title Talent DAO NFT Contract
 /// @author Jaxcoder
-/// @dev ERC721 to represent articles submitted by authors as the IP and license
+/// @dev ERC1155 to represent articles submitted by authors as the IP and license
 contract rTalentNftToken is Ownable, ERC1155 {
     using Counters for Counters.Counter;
-    using SafeERC20 for IERC20;
 
     Counters.Counter private _tokenIds;
 
-    constructor(address _owner, string memory tokenURI) public ERC1155("") {
+    constructor(address _owner, string memory tokenURI) ERC1155("") {
         _transferOwnership(_owner);
     }
 
@@ -30,7 +27,7 @@ contract rTalentNftToken is Ownable, ERC1155 {
     /// @dev this is internal mint function
     /// @param ownerAddress the user that is minting the token address
     /// @param tokenURI the uri for the token metadata
-    function mintNFT(address ownerAddress, string memory tokenURI)
+    function mintNFT(address ownerAddress, string memory tokenURI, bytes memory data)
         public
         returns (uint256)
     {
@@ -38,7 +35,7 @@ contract rTalentNftToken is Ownable, ERC1155 {
 
         // mint the nft to the author/owner
         uint256 newItemId = _tokenIds.current();
-        // _mint(ownerAddress, newItemId);
+        _mint(ownerAddress, newItemId, 1, data);
 
         // return the tokenId and the authorId it was minted to
         // the authorId will be 
