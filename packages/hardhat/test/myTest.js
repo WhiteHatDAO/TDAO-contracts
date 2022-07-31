fde9bd72f6985ba20f83a612dc80e8ad5dfd5624
MantisClone <david.hunt-mateo@mantisdata.solutions>
2022-07-31 08:47:41 -0400

    Fix tests, deploy ReputationController

--- packages/hardhat/test/myTest.js
+++ packages/hardhat/test/myTest.js
@@ -4,28 +4,27 @@ const { solidity } = require("ethereum-waffle");
 
 use(solidity);
 
-describe("My Dapp", function () {
-  let myContract;
+describe("TalentDAO Journal of Decentralized Work", function () {
+  let contract;
 
   // quick fix to let gas reporter fetch data from gas station & coinmarketcap
   before((done) => {
     setTimeout(done, 2000);
   });
 
-  describe("YourContract", function () {
-    it("Should deploy YourContract", async function () {
-      const YourContract = await ethers.getContractFactory("YourContract");
-
-      myContract = await YourContract.deploy();
+  describe("ReputationController", function () {
+    it("Should deploy ReputationController", async function () {
+      const Contract = await ethers.getContractFactory("ReputationController");
+      contract = await Contract.deploy();
     });
 
-    describe("setPurpose()", function () {
-      it("Should be able to set a new purpose", async function () {
-        const newPurpose = "Test Purpose";
+    // describe("createNewUser()", function () {
+    //   it("Should create a new user", async function () {
+    //     const newPurpose = "Test Purpose";
 
-        await myContract.setPurpose(newPurpose);
-        expect(await myContract.purpose()).to.equal(newPurpose);
-      });
+    //     await myContract.setPurpose(newPurpose);
+    //     expect(await myContract.purpose()).to.equal(newPurpose);
+    //   });
 
       // Uncomment the event and emit lines in YourContract.sol to make this test pass
 
@@ -38,6 +37,6 @@ describe("My Dapp", function () {
           emit(myContract, "SetPurpose").
             withArgs(owner.address, newPurpose);
       });*/
-    });
+    // });
   });
 });
