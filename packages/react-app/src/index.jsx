5bb4f93c8981a424aedaa1bb1b3f3d4feef07176
Jaxcoder <codenamejason@outlook.com>
2022-08-14 12:18:48 -0400

    add wagmi config

--- packages/react-app/src/index.jsx
+++ packages/react-app/src/index.jsx
@@ -3,9 +3,11 @@ import React from "react";
 import { ThemeSwitcherProvider } from "react-css-theme-switcher";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { WagmiConfig } from "wagmi";
 import App from "./App";
 import { ErrorBoundary } from "./components";
 import "./index.css";
+import client from "./utils/wagmi";
 
 const themes = {
   dark: "./dark-theme.css",
@@ -16,18 +18,20 @@ const prevTheme = window.localStorage.getItem("theme");
 
 const subgraphUri = "https://arweave.net/graphql";
 
-const client = new ApolloClient({
+const apolloClient = new ApolloClient({
   uri: subgraphUri,
   cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
   <ErrorBoundary>
-    <ApolloProvider client={client}>
+    <ApolloProvider client={apolloClient}>
       <ThemeSwitcherProvider themeMap={themes} defaultTheme={prevTheme || "light"}>
-        <Router>
-          <App subgraphUri={subgraphUri} />
-        </Router>
+        <WagmiConfig client={client}>
+          <Router>
+            <App subgraphUri={subgraphUri} />
+          </Router>
+        </WagmiConfig>
       </ThemeSwitcherProvider>
     </ApolloProvider>
   </ErrorBoundary>,
