bc3e19c47f83bc6e7d9af29604357ae8011a49d3
Jaxcoder <codenamejason@outlook.com>
2022-09-05 22:19:54 -0400

    update access based on logged in or not

--- packages/react-app/src/views/User.jsx
+++ packages/react-app/src/views/User.jsx
@@ -1,3 +1,4 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useAccount } from "wagmi";
@@ -8,16 +9,15 @@ import {
   ReputationPage,
   RewardsPage,
   UserArticles,
-  UserConnect,
-  UserSubmissions,
+  UserSubmissions
 } from "../components";
 
 const configUserType = {
   none: -1,
   submission: 0,
   article: 1,
-  edit_profile: 2,
-  logout: 3,
+  editProfile: 2,
+  wallet: 3,
   notifications: 4,
   publisher: 5,
   reputation: 6,
@@ -26,7 +26,7 @@ const configUserType = {
 
 export default function User({ userMenuOpen, handleUserMenuOpen }) {
   const [, setMenuOpen] = useState(userMenuOpen);
-  const [userConfig, setUserConfig] = useState(configUserType.none);
+  const [userConfig, setUserConfig] = useState(configUserType.editProfile);
   const location = useLocation();
   const navigate = useNavigate();
 
@@ -42,7 +42,7 @@ export default function User({ userMenuOpen, handleUserMenuOpen }) {
       navigate("/user/submissions");
     } else if (type === configUserType.article) {
       navigate("/user/articles");
-    } else if (type === configUserType.edit_profile) {
+    } else if (type === configUserType.editProfile) {
       navigate("/user/author");
     } else if (type === configUserType.notifications) {
       navigate("/user/notifications");
@@ -59,24 +59,24 @@ export default function User({ userMenuOpen, handleUserMenuOpen }) {
 
   useEffect(() => {
     if (address === undefined) {
-      setUserConfig(configUserType.none);
+      setUserConfig(configUserType.editProfile);
     } else {
       if (location.pathname.includes("/submissions")) setUserConfig(configUserType.submission);
       else if (location.pathname.includes("/articles")) setUserConfig(configUserType.article);
-      else if (location.pathname.includes("/author")) setUserConfig(configUserType.edit_profile);
+      else if (location.pathname.includes("/author")) setUserConfig(configUserType.editProfile);
       else if (location.pathname.includes("/notifications")) setUserConfig(configUserType.notifications);
       else if (location.pathname.includes("/publisher")) setUserConfig(configUserType.publisher);
       else if (location.pathname.includes("/reputation")) setUserConfig(configUserType.reputation);
       else if (location.pathname.includes("/rewards")) setUserConfig(configUserType.rewards);
       else setUserConfig(configUserType.submission);
     }
-  }, [address]);
+  }, [address, location.pathname]);
 
   useEffect(() => {
     if (location.pathname.includes("/submissions")) setUserConfig(configUserType.submission);
     else if (location.pathname.includes("/articles")) setUserConfig(configUserType.article);
     else if (location.pathname.includes("/author")) {
-      setUserConfig(configUserType.edit_profile);
+      setUserConfig(configUserType.editProfile);
     } else if (location.pathname.includes("/notifications")) setUserConfig(configUserType.notifications);
     else if (location.pathname.includes("/publisher")) setUserConfig(configUserType.publisher);
     else if (location.pathname.includes("/reputation")) setUserConfig(configUserType.reputation);
@@ -85,22 +85,6 @@ export default function User({ userMenuOpen, handleUserMenuOpen }) {
 
   const Menu = () => (
     <>
-      <div className="pl-8 pr-2 py-2 lg:hidden flex flex-row items-center justify-between text-lg text-primary">
-        <div>Connect</div>
-        <svg
-          width="23"
-          height="23"
-          viewBox="0 0 23 23"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          onClick={handleMenuOpen}
-        >
-          <path
-            d="M14.4785 6.9L11.5 9.8785L8.5215 6.9L6.9 8.5215L9.8785 11.5L6.9 14.4785L8.5215 16.1L11.5 13.1215L14.4785 16.1L16.1 14.4785L13.1215 11.5L16.1 8.5215L14.4785 6.9ZM11.5 0C5.1405 0 0 5.1405 0 11.5C0 17.8595 5.1405 23 11.5 23C17.8595 23 23 17.8595 23 11.5C23 5.1405 17.8595 0 11.5 0ZM11.5 20.7C6.4285 20.7 2.3 16.5715 2.3 11.5C2.3 6.4285 6.4285 2.3 11.5 2.3C16.5715 2.3 20.7 6.4285 20.7 11.5C20.7 16.5715 16.5715 20.7 11.5 20.7Z"
-            fill="#656565"
-          />
-        </svg>
-      </div>
       <div
         className="rounded-md bg-transparent hover:bg-gray px-8 py-2 flex flex-row items-center cursor-pointer text-lg"
         onClick={() => handleConfigTypeChanged(configUserType.submission)}
@@ -167,27 +151,29 @@ export default function User({ userMenuOpen, handleUserMenuOpen }) {
       </div>
       <div
         className="rounded-md bg-transparent hover:bg-gray px-8 py-2 flex flex-row items-center cursor-pointer text-lg"
-        onClick={() => handleConfigTypeChanged(configUserType.edit_profile)}
+        onClick={() => handleConfigTypeChanged(configUserType.editProfile)}
       >
         <svg width="35" height="35" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg" className="pr-3">
           <path
             d="M0 27.7093V35H7.29065L28.7932 13.4974L21.5026 6.20678L0 27.7093ZM34.4313 7.85932C35.1896 7.1011 35.1896 5.87627 34.4313 5.11804L29.882 0.568671C29.1237 -0.189557 27.8989 -0.189557 27.1407 0.568671L23.5828 4.12651L30.8735 11.4172L34.4313 7.85932Z"
-            fill={userConfig === configUserType.edit_profile ? "#B41C2E" : "#929292"}
+            fill={userConfig === configUserType.editProfile ? "#B41C2E" : "#929292"}
           />
         </svg>
-        <div className={userConfig === configUserType.edit_profile ? "text-primary" : "text-lightgray"}>Profile</div>
+        <div className={userConfig === configUserType.editProfile ? "text-primary" : "text-lightgray"}>Profile</div>
       </div>
       <div
-        className="rounded-md bg-transparent hover:bg-gray px-8 py-2 flex flex-row items-center cursor-pointer text-lg"
-        onClick={() => handleConfigTypeChanged(configUserType.logout)}
+        className="rounded-md bg-transparent px-8 py-2 flex flex-row items-center cursor-pointer text-lg"
+        onClick={() => handleConfigTypeChanged(configUserType.wallet)}
       >
-        <svg width="35" height="35" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg" className="pr-3">
-          <path
-            d="M21.2487 10.6663H19.7904V7.74967C19.7904 3.72467 16.5237 0.458008 12.4987 0.458008C8.4737 0.458008 5.20703 3.72467 5.20703 7.74967V10.6663H3.7487C2.14453 10.6663 0.832031 11.9788 0.832031 13.583V28.1663C0.832031 29.7705 2.14453 31.083 3.7487 31.083H21.2487C22.8529 31.083 24.1654 29.7705 24.1654 28.1663V13.583C24.1654 11.9788 22.8529 10.6663 21.2487 10.6663ZM12.4987 23.7913C10.8945 23.7913 9.58203 22.4788 9.58203 20.8747C9.58203 19.2705 10.8945 17.958 12.4987 17.958C14.1029 17.958 15.4154 19.2705 15.4154 20.8747C15.4154 22.4788 14.1029 23.7913 12.4987 23.7913ZM17.0195 10.6663H7.97786V7.74967C7.97786 5.25592 10.0049 3.22884 12.4987 3.22884C14.9924 3.22884 17.0195 5.25592 17.0195 7.74967V10.6663Z"
-            fill={userConfig === configUserType.logout ? "#B41C2E" : "#929292"}
+        {address ? (
+          <ConnectButton
+            showBalance={false}
+            chainStatus={{ smallScreen: "icon", largeScreen: "icon" }}
+            accountStatus={{ largeScreen: "avatar", smallScreen: "avatar" }}
           />
-        </svg>
-        <div className={userConfig === configUserType.logout ? "text-primary" : "text-lightgray"}>Log Out</div>
+        ) : (
+          <ConnectButton />
+        )}
       </div>
     </>
   );
@@ -212,7 +198,7 @@ export default function User({ userMenuOpen, handleUserMenuOpen }) {
           <div className="w-full ">
             {userConfig === configUserType.none ? (
               <div className="flex justify-center">
-                <UserConnect></UserConnect>
+                <div className="flex flex-col items-center">Hello</div>
               </div>
             ) : userConfig === configUserType.submission ? (
               <div className="flex flex-col">
@@ -222,9 +208,9 @@ export default function User({ userMenuOpen, handleUserMenuOpen }) {
               <div className="flex flex-col">
                 <UserArticles address={address}></UserArticles>
               </div>
-            ) : userConfig === configUserType.edit_profile ? (
+            ) : userConfig === configUserType.editProfile ? (
               <div className="flex flex-col">
-                <EditUserProfile address={address}></EditUserProfile>
+                <EditUserProfile />
               </div>
             ) : userConfig === configUserType.notifications ? (
               <div className="flex flex-col">
